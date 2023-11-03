dc36d531aa9f78ae48eb50517225b9c8c542e731
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-04 04:18:22 +0600

    Создал функцию которая считает количество ингридиентов

--- src/Ingredients.tsx
+++ src/Ingredients.tsx
@@ -2,53 +2,71 @@ import baconImg from './assets/bacon.png';
 import saladImg from './assets/salad.png';
 import cheeseImg from './assets/cheese.png';
 import cutletImg from './assets/cutlet.png';
-import './App.css';
+import {useState} from "react";
 
 function Ingredients() {
+    const [products, setProducts] = useState<{
+        bacon: number;
+        salad: number;
+        cheese: number;
+        cutlet: number;
+    }>({
+        bacon: 0,
+        salad: 0,
+        cheese: 0,
+        cutlet: 0,
+    });
+
+    const increaseQuantity = (product) => {
+        setProducts({
+            ...products,
+            [product]: products[product] + 1,
+        });
+    }
+
+    const decreaseQuantity = (product) => {
+        if (products[product] > 0) {
+            setProducts({
+                ...products,
+                [product]: products[product] - 1,
+            });
+        }
+    }
+
 
     return (
-        <div className="orderWindow">
-            <div className="Burger">
-                <div className="BreadTop">
-                    <div className="Seeds1"></div>
-                    <div className="Seeds2"></div>
-                </div>
-                <div className="Salad"></div>
-                <div className="Cheese"></div>
-                <div className="Meat"></div>
-                <div className="BreadBottom"></div>
-            </div>
+
+
             <div className="addIngredientsBlock">
                 <div className="IngredientsBtn">
                     <img className="ingredient" src={baconImg} alt="bacon"/>
                     <span>Bacon</span>
-                    <span>x1</span>
-                    <button>+</button>
-                    <button>-</button>
+                    <span>{products.bacon}</span>
+                    <button onClick={() => increaseQuantity('bacon')}>+</button>
+                    <button onClick={() => decreaseQuantity('bacon')}>-</button>
                 </div>
                 <div className="IngredientsBtn">
                     <img className="ingredient" src={saladImg} alt="salad"/>
                     <span>Salad</span>
-                    <span>x1</span>
-                    <button>+</button>
-                    <button>-</button>
+                    <span>{products.salad}</span>
+                    <button onClick={() => increaseQuantity('salad')}>+</button>
+                    <button onClick={() => decreaseQuantity('salad')}>-</button>
                 </div>
                 <div className="IngredientsBtn">
                     <img className="ingredient" src={cutletImg} alt="cutlet"/>
                     <span>Cutlet</span>
-                    <span>x1</span>
-                    <button>+</button>
-                    <button>-</button>
+                    <span>{products.cutlet}</span>
+                    <button onClick={() => increaseQuantity('cutlet')}>+</button>
+                    <button onClick={() => decreaseQuantity('cutlet')}>-</button>
                 </div>
                 <div className="IngredientsBtn">
                     <img className="ingredient" src={cheeseImg} alt="cheese"/>
                     <span>Cheese</span>
-                    <span>x1</span>
-                    <button>+</button>
-                    <button>-</button>
+                    <span>{products.cheese}</span>
+                    <button onClick={() => increaseQuantity('cheese')}>+</button>
+                    <button onClick={() => decreaseQuantity('cheese')}>-</button>
                 </div>
             </div>
-        </div>
     )
 }
 
