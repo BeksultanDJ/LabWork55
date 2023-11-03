fcc0dd4bc31305aea21a93d076fae0d2033f1eaf
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-04 02:52:53 +0600

    Импортировал файд Ingredients

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
+import './App.css';
+import Ingredients from './Ingredients';
 
 function App() {
 
 
   return (
     <>
-
+      <Ingredients/>
     </>
   )
 }
 
-export default App
+export default App;
