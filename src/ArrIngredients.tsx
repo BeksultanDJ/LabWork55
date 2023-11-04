89f90e9e740d2e2d536ce0712566df990296cb69
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-04 06:06:29 +0600

    Создал массив ингредиентов

---
+++ src/ArrIngredients.tsx
@@ -0,0 +1,11 @@
+import baconImg from './assets/bacon.png';
+import saladImg from './assets/salad.png';
+import cheeseImg from './assets/cheese.png';
+import cutletImg from './assets/cutlet.png';
+
+export const INGREDIENTS = [
+    { name: 'Bacon', price: 10, image: baconImg },
+    { name: 'Salad', price: 5, image: saladImg },
+    { name: 'Meat', price: 15, image: cutletImg },
+    { name: 'Cheese', price: 8, image: cheeseImg },
+];
